77f600681c9a743755df981f7cb442ddbd4da6ca
Danny <kalgon600@gmail.com>
2025-12-15 14:39:06 +0100

    Parę poprawek do poprzedniego

--- Projekt_Windy/src/main/java/com/example/projekt_windy/BudynekApplication.java
+++ Projekt_Windy/src/main/java/com/example/projekt_windy/BudynekApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class BudynekApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Budynek.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(BudynekApplication.class.getResource("Budynek.fxml"));
         Scene scene = new Scene(fxmlLoader.load(),600,400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
